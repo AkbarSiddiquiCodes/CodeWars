95227c764b896b63be36d65f475fb47e8901b871
Akbar Siddiqui <akbarnoire@gmail.com>
2024-05-29 16:59:35 +0530

    CountByX.sol
    Count By X is the first challenge on CodeWars.

---
+++ CountByX.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+//Create a function with two arguments that will return an array of the first n multiples of x.
+
+//Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+//Return the results as an array or list ( depending on language ).
+
+//Examples
+//countBy(1,10) // should return [1,2,3,4,5,6,7,8,9,10]
+//countBy(2,5) // should return [2,4,6,8,10]
+
+contract CountByX {
+
+  function countBy(int x, int n) public pure returns (int[] memory) {
+    int[] memory z = new int[](uint(n));
+    // TODO: Your code here
+    for(int i = 0; i < n; i++){
+        z[uint(i)] = x * (i + 1);
+    }   
+    return z;
+  }
+}
