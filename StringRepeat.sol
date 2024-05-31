9f6bbd004aa9e0af3d7d03611c7dcb1445641627
Akbar Siddiqui <akbarnoire@gmail.com>
2024-05-31 17:31:08 +0530

    StringRepeat.sol

---
+++ StringRepeat.sol
@@ -0,0 +1,19 @@
+//Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+//Examples (input -> output)
+//6, "I"     -> "IIIIII"
+//5, "Hello" -> "HelloHelloHelloHelloHello"
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.25;
+
+contract Repeater {
+  function multiply(uint8 repeat, string memory pattern) public pure returns (string memory) {
+    string memory script = "";
+        for (uint8 i = 0; i < repeat; i++) {
+            script = string.concat(script, pattern);
+        }
+    return script;
+  }
+}
