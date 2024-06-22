c34d11811589abe35cc1e2e19494b12bd5ee8254
Akbar Siddiqui <akbarnoire@gmail.com>
2024-06-22 17:22:30 +0530

    GrasshopperTerminal.sol

---
+++ GrasshopperTerminal.sol
@@ -0,0 +1,18 @@
+// Terminal game move function
+// In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+
+// Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+
+// Example:
+// move(3, 6) should equal 15
+
+
+// SPDX-License-Identifier: BSD-2-Clause
+pragma solidity ^0.8.16;
+
+contract Kata {
+  function move(int p, int r) public pure returns (int) {
+    int ans = p + r + r;
+    return(ans);
+  }
+}
