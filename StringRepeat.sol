6a747d70293066743c8ba19fe54b8311224d8435
Akbar Siddiqui <akbarnoire@gmail.com>
2024-06-22 17:16:52 +0530

    StringRepeat.sol

--- StringRepeat.sol
+++ StringRepeat.sol
@@ -4,8 +4,8 @@
 //6, "I"     -> "IIIIII"
 //5, "Hello" -> "HelloHelloHelloHelloHello"
 
-// SPDX-License-Identifier: MIT
 
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
 contract Repeater {
